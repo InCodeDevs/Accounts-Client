1ec89cc36ee80957379bb4c770671989cbcaa10b
MCTzOCK <dev@mctzock.de>
2021-10-11 20:19:37 +0200

    Fixed Imports

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@
  * @author Ben Siebert <ben@mctzock.de>
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
+
+const {WebClient} = require('./lib/WebClient');
+
+module.exports = {
+    WebClient
+}
